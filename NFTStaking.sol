0f432a7d196b79491c404d335dcfee4877034467
Burtininkas69 <gustasklisauskas@gmail.com>
2022-06-08 01:29:17 +0300

    aaaaaaaa

--- NFTStaking.sol
+++ NFTStaking.sol
@@ -1,188 +1,101 @@
 // SPDX-License-Identifier: GPL-3.0
+pragma solidity ^ 0.8 .4;
 
-pragma solidity >= 0.7 .0 < 0.9 .0;
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
+/// @notice creates an interface for SafeTransfer function used to transfer NFTs.
+interface Interface {
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+}
 
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
-import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+contract Coin is IERC721Receiver {
 
-contract NFT is ERC721Enumerable, Ownable, VRFConsumerBaseV2 {
-    VRFCoordinatorV2Interface COORDINATOR;
-    using Strings
-    for uint256;
+    address public parentNFT;
+    address public owner;
 
+    struct NFTInfo {
+        uint ID;
+        uint timer;
+        address owner;
+        bool staked;
+    }
 
-    string public baseURI;
-    string public notRevealedUri;
-    string public baseExtension = ".json";
+    NFTInfo[] public NFTInformation;
 
-    /// @notice edit these before launching contract
-    /// @dev this information is not editable after launch
-    uint8 public referralRewardPercentage = 20;
-    uint16 public nonce = 1;
+    mapping(address => uint) public tokensOwned;
+    mapping(address => uint[]) public ownerOfPositions;
+    mapping(address => bool) public whitelisted;
+    mapping(uint => uint) public NFTsPosition;
+    mapping(uint => bool) public NFTStaked;
 
-    uint16 constant public maxSupply = 10;
+    constructor(address _parentNFTAddress) {
+        parentNFT = _parentNFTAddress;
+        owner = msg.sender;
+    }
 
-
-    /// @notice CHAINLINK
-    uint64 s_subscriptionId;
-    address vrfCoordinator = 0x6168499c0cFfCaCD319c818142124B7A15E857ab;
-    bytes32 keyHash = 0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc;
-    uint32 callbackGasLimit = 100000;
-    uint16 requestConfirmations = 3;
-    uint32 numWords = 2;
-
-    uint256[] public s_randomWords;
-    uint256 public s_requestId;
-    address s_owner;
-
-    uint256 public cost = 0.05 ether;
-
-    /// @notice this uint prevents owners from withdrawing referral payouts from contract
-    uint256 internal referralObligationPool;
-
-    bool public paused = false;
-    bool public revealed = false;
-    bool public frozenURI = false;
-
-
-    /// @notice used to find unminted ID
-    uint16[maxSupply] public mints;
-
-    mapping(string => bool) public codeIsTaken;
-    mapping(string => address) internal ownerOfCode;
-
-    constructor(string memory _name, string memory _symbol, string memory _unrevealedURI, uint64 subscriptionId) ERC721(_name, _symbol) VRFConsumerBaseV2(vrfCoordinator) {
-        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinator);
-        s_owner = msg.sender;
-        s_subscriptionId = subscriptionId;
-        setUnrevealedURI(_unrevealedURI);
-
-        /// @notice an array of lets say 10,000 numbers would be too long to hardcode it. So we are using constructor to generate all numbers for us.
-        /// @dev generates all possible IDs of NFTs that our findUnminted() function picks from
-        for (uint16 i = 0; i < maxSupply; ++i) {
-            mints[i] = i;
+    function updateInformation() public {
+        for (uint i; i < NFTInformation.length; i++) {
+            if ((block.timestamp - NFTInformation[i].timer ) >= 30 && NFTInformation[i].staked) {
+                tokensOwned[NFTInformation[i].owner] = tokensOwned[NFTInformation[i].owner] + ((block.timestamp - NFTInformation[i].timer) / 30 * 25);
+                NFTInformation[i].timer = NFTInformation[i].timer + (block.timestamp - NFTInformation[i].timer);
+            }
         }
     }
 
-    modifier notPaused {
-        require(!paused);
-        _;
-    }
+    function stake(uint[] memory _id) public {
+        for (uint i; i < _id.length; i++) {
 
-    function _baseURI() internal view virtual override returns(string memory) {
-        return baseURI;
-    }
+            Interface(parentNFT).safeTransferFrom(msg.sender, address(this), _id[i]);
 
-    // Assumes the subscription is funded sufficiently.
-    function requestRandomWords() public returns(uint8) {
-        // Will revert if subscription is not set and funded.
-        uint8 _requestId = uint8(COORDINATOR.requestRandomWords(
-            keyHash,
-            s_subscriptionId,
-            requestConfirmations,
-            callbackGasLimit,
-            numWords
-        ));
-        return _requestId;
-    }
-
-    function fulfillRandomWords(
-        uint256, /* requestId */
-        uint256[] memory randomWords
-    ) internal override {
-        s_randomWords = randomWords;
-    }
-
-    /// @notice calls for random number and picks corresponding NFT
-    /// @dev deletes used ID from array and moves the last uint to it's place. Also shortens arrays length, so it would be impossible to pick same number.
-    function findUnminted() internal returns(uint16) {
-        uint16 supply = uint16(totalSupply());
-        uint16 index = requestRandomWords() % (maxSupply - supply);
-        uint16 chosenNft = mints[index];
-        uint16 swapNft = mints[maxSupply - supply - 1];
-        mints[index] = uint16(swapNft);
-        return (chosenNft);
-    }
-
-    /// @notice mint function with referral code to give user discount and pay referral
-    /// @dev function has an extra input - string. It is used for referral code. If the user does not put any code, string looks like this "".
-    function mint(address _to, uint256 _mintAmount) public payable notPaused {
-        uint256 supply = totalSupply();
-        require(_mintAmount > 0);
-
-        require(supply + _mintAmount <= maxSupply);
-        if (s_owner != msg.sender) {
-            require(msg.value >= cost * _mintAmount, "MintWithoutReferral: Not enough ether");
-        }
-
-        for (uint256 i = 0; i < _mintAmount; i++) {
-            _safeMint(_to, findUnminted());
+            if (!NFTStaked[_id[i]]) {
+                NFTInfo memory newNFT = NFTInfo(_id[i], block.timestamp, msg.sender, true);
+                NFTInformation.push(newNFT);
+                NFTsPosition[_id[i]] = NFTInformation.length - 1;
+            } else {
+                NFTInformation[NFTsPosition[_id[i]]].timer = block.timestamp;
+                NFTInformation[NFTsPosition[_id[i]]].owner = msg.sender;
+                NFTInformation[NFTsPosition[_id[i]]].staked = true;
+            }
+            ownerOfPositions[msg.sender].push(NFTsPosition[_id[i]]);
+            NFTStaked[_id[i]] = true;
         }
     }
 
+    function unstake(uint[] memory _id) public {
+        for (uint i; i < _id.length; i++) {
+            require(NFTStaked[_id[i]], "NFT is not in the contract");
+            require(NFTInformation[NFTsPosition[_id[i]]].owner == msg.sender, "You are not the owner of this NFT");
+            Interface(parentNFT).safeTransferFrom(address(this), msg.sender, _id[i]);
 
-    function walletOfOwner(address _owner) public view returns(uint256[] memory) {
-        uint256 ownerTokenCount = balanceOf(_owner);
-        uint256[] memory tokenIds = new uint256[](ownerTokenCount);
-
-        for (uint256 i; i < ownerTokenCount; i++) {
-            tokenIds[i] = tokenOfOwnerByIndex(_owner, i);
+            NFTInformation[NFTsPosition[_id[i]]].staked = false;
+            delete ownerOfPositions[msg.sender][NFTsPosition[_id[i]]];Q
+            NFTStaked[_id[i]] = false;
         }
-
-        return tokenIds;
     }
 
-    function tokenURI(uint256 tokenId) public view virtual override returns(string memory) {
-        require(_exists(tokenId),
-            "ERC721Metadata: URI query for nonexistent token"
-        );
+    function timeNow() public view returns(uint) {
+        return (block.timestamp);
+    }
 
-        if (revealed == false) {
-            return notRevealedUri;
+    function timeDivision() public view returns(uint) {
+        uint temp;
+        for (uint i; i < ownerOfPositions[msg.sender].length; i++) {
+            temp = temp + NFTInformation[ownerOfPositions[msg.sender][i]].timer;
         }
-
-        string memory currentBaseURI = _baseURI();
-        return bytes(currentBaseURI).length > 0 ? string(abi.encodePacked(currentBaseURI, tokenId.toString(), baseExtension)) : "";
+        return (temp);
     }
 
-    //only owner
-
-
-    function freezeURI() public onlyOwner {
-        frozenURI = true;
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) public override returns(bytes4) {
+        return this.onERC721Received.selector;
     }
 
-    function setBaseURI(string memory _newBaseURI) public onlyOwner {
-        require(!frozenURI, "URI is frozen");
-        baseURI = _newBaseURI;
+    function whitelistContract(address _address, bool _bool) public {
+        whitelisted[_address] = _bool;
     }
 
-    function setUnrevealedURI(string memory _unrevealedURI) public onlyOwner {
-        require(!frozenURI, "URI is frozen");
-        notRevealedUri = _unrevealedURI;
-    }
-
-    function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
-        baseExtension = _newBaseExtension;
-    }
-
-    function reveal() public onlyOwner {
-        revealed = true;
-    }
-
-    /// @notice emergency pause. If something goes wrong, we could pause the mint function
-    function pause(bool _state) public onlyOwner {
-        paused = _state;
-    }
-
-    /// @notice balance is split 50/50 to founder & coFounder
-    function withdraw() public onlyOwner {
-        (bool oc, ) = payable(owner()).call {
-            value: address(this).balance
-        }("");
-        require(oc);
+    function subtractTokens(address _address, uint _tokens) public {
+        require(msg.sender == owner || whitelisted[msg.sender]);
+        require(_tokens <= tokensOwned[_address]);
+        tokensOwned[_address] = tokensOwned[_address] - _tokens;
     }
 }
---
+++ staker3.0.sol
@@ -0,0 +1,110 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^ 0.8 .4;
+
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+
+/// @notice creates an interface for SafeTransfer function used to transfer NFTs.
+interface Interface {
+    function safeTransferFrom(address from, address to, uint256 tokenId) external;
+}
+
+
+contract Staker is IERC721Receiver, KeeperCompatibleInterface {
+
+
+    address public parentNFT;
+    address public owner;
+
+    uint public immutable interval;
+    uint public lastTimeStamp;
+
+    uint[] public stakedNFTs;
+
+    mapping(uint => bool) public isStaked;
+    mapping(uint => uint) public lastSnap;
+    mapping(uint => uint) public arrayPosition;
+    mapping(uint => uint) public stakedPosition;
+    mapping(uint => address) public ownerOf;
+    mapping(address => uint) public tokensCollected;
+    mapping(address => uint[]) public tokensOwned;
+    mapping(address => bool) public whitelisted;
+
+
+    mapping(uint => uint) public test;
+
+    constructor(address _parentNFTAddress, uint updateInterval) {
+        parentNFT = _parentNFTAddress;
+        owner = msg.sender;
+        interval = updateInterval;
+        lastTimeStamp = block.timestamp;
+    }
+
+    function checkUpkeep(bytes calldata /* checkData */ ) external view override returns(bool upkeepNeeded, bytes memory /* performData */ ) {
+        upkeepNeeded = (block.timestamp - lastTimeStamp) > interval;
+        // We don't use the checkData in this example. The checkData is defined when the Upkeep was registered.
+    }
+
+    function performUpkeep(bytes calldata /* performData */ ) external override {
+        updateAll();
+    }
+
+    function stake(uint[] memory _IDs) public {
+        for (uint i; i < _IDs.length; i++) {
+            require(!isStaked[_IDs[i]]);
+            uint _tempID = _IDs[i];
+            Interface(parentNFT).safeTransferFrom(msg.sender, address(this), _tempID);
+            isStaked[_tempID] = true;
+            lastSnap[_tempID] = block.timestamp;
+            ownerOf[_tempID] = msg.sender;
+            arrayPosition[_tempID] = tokensOwned[msg.sender].length;
+            stakedPosition[_tempID] = stakedNFTs.length;
+            tokensOwned[msg.sender].push(_tempID);
+            stakedNFTs.push(_tempID);
+        }
+    }
+
+    function unstake(uint[] memory _IDs) public {
+        for (uint i; i < _IDs.length; i++) {
+            require(isStaked[_IDs[i]]);
+            uint _tempID = _IDs[i];
+            uint tempPosition = stakedPosition[_tempID];
+            uint tempLastPosition = stakedNFTs.length - 1;
+            isStaked[_tempID] = false;
+            delete tokensOwned[msg.sender][arrayPosition[_tempID]];
+            stakedPosition[tempLastPosition] = stakedNFTs[tempPosition];
+            stakedNFTs[tempPosition] = stakedNFTs[tempLastPosition];
+            delete stakedNFTs[tempLastPosition];
+            Interface(parentNFT).safeTransferFrom(address(this), msg.sender, _tempID);
+        }
+    }
+
+    function updateAll() public {
+        for (uint i; i < stakedNFTs.length; i++) {
+            tokensCollected[ownerOf[stakedNFTs[i]]]++;
+        }
+        lastTimeStamp = block.timestamp;
+    }
+
+    function ownerOfToknes(address _address) public view returns(uint[] memory) {
+        uint[] memory _tokens;
+        for (uint i; i < tokensOwned[_address].length; i++) {
+            _tokens[i] = tokensOwned[_address][i];
+        }
+        return (_tokens);
+    }
+
+    function onERC721Received(address operator, address from, uint256 tokenId, bytes calldata data) public override returns(bytes4) {
+        return this.onERC721Received.selector;
+    }
+
+    function whitelistContract(address _address, bool _bool) public {
+        whitelisted[_address] = _bool;
+    }
+
+    function subtractTokens(address _address, uint _tokens) public {
+        require(msg.sender == owner || whitelisted[msg.sender]);
+        require(_tokens <= tokensCollected[_address]);
+        tokensCollected[_address] = tokensCollected[_address] - _tokens;
+    }
+}
